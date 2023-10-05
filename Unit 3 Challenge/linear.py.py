caff39614b0f2ff042e1d041eaba245978ada704
vinoth22h301 <146090082+vinoth22h301@users.noreply.github.com>
2023-10-05 17:09:00 +0530

    Add files via upload

---
+++ Unit 3 Challenge/linear.py.py
@@ -0,0 +1,12 @@
+def linearSearchProduct(productList, targetProduct):
+  Indices = []
+ 
+  for index, product in enumerate(productList)
+     if product == targetProduct:
+         Indices.appand(index)
+   return indices
+Products = ["shoes","boot","loafer",shoes","sandal","shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products , target)
+Print(result)￼Not
